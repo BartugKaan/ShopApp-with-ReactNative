d15dfbe0365b7d24d67ea56cf583cf081d4fca43
Bartuğ Kaan  Çelebi <bartugkaan.celebi@outlook.com>
2024-12-04 18:07:12 +0300

    add: Product

---
+++ src/data/products.js
@@ -0,0 +1,36 @@
+export const products = [
+  {
+    "_id": {
+      "$oid": "62e13fd7164339d544819593"
+    },
+    "vendor": {
+      "name": "Sobe Istanbul"
+    },
+    "series": {
+      "name": "2S-2M-2L",
+      "item_quantity": 6
+    },
+    "description_details": {
+      "en": {
+        "fabric": "Premium cotton blend",
+        "model_measurements": "",
+        "sample_size": "",
+        "product_measurements": ""
+      }
+    },
+    "main_image": "https://loncapazar.s3.eu-north-1.amazonaws.com/product/Sobe/2016580-1",
+    "price": 3,
+    "names": {
+      "en": "16580 - Blouse - Beige"
+    },
+    "images": [
+      "https://loncapazar.s3.eu-north-1.amazonaws.com/product/Sobe/2016580-1",
+      "https://loncapazar.s3.eu-north-1.amazonaws.com/product/Sobe/2016580-2",
+      "https://loncapazar.s3.eu-north-1.amazonaws.com/product/Sobe/2016580-3",
+      "https://loncapazar.s3.eu-north-1.amazonaws.com/product/Sobe/2016580-4",
+      "https://loncapazar.s3.eu-north-1.amazonaws.com/product/Sobe/2016580-5",
+      "https://loncapazar.s3.eu-north-1.amazonaws.com/product/Sobe/2016580-6"
+    ],
+    "product_code": "16580"
+  }
+]; 
